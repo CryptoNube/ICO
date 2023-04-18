83bc68fa2bc97074febc9563bfe5cf3d215d65f8
CryptoNube <daverad@hotmail.com>
2023-04-18 17:03:16 -0500

    initial commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  "0x6bFd368BB305fc49e0de66CFbAde43279c622Cdc";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
