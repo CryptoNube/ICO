a9635252c297edb572658c86d71de5e71e2ccc0e
CryptoNube <daverad@hotmail.com>
2023-04-19 17:44:45 -0500

    replace contract address with correct address

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,5 +1,5 @@
 // Address of the NFT Contract that you deployed
 const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
-  "0x6bFd368BB305fc49e0de66CFbAde43279c622Cdc";
+  "0xcb2b47d1906258Bf66cbfd35Fb824547BcEa08C9";
 
 module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
